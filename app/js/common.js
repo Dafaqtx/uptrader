bf7f013a5271a3ab70cfefe4ad5288ee6093b069
Alexander Kuchuk <dafaqtx@gmail.com>
2018-12-19 17:46:19 +0300

    add mobile version

--- app/js/common.js
+++ app/js/common.js
@@ -1,4 +1,8 @@
 $(function() {
+
+	const getWidth = function() {
+		return $(window).width();
+	}
 	
 	function tabs(item) {
 		let $item       = $(item);
@@ -12,11 +16,39 @@ $(function() {
 
 		tabsContent.removeClass('type-tabs__content--active');
 		$('.type-tabs__content:eq(' + index + ')').addClass('type-tabs__content--active');
+	}
 
+	function mobileChose(item) {
+		let width = getWidth();
+
+		if ( width < 1200 ) {
+			let $item       = $(item);
+			let outBlock    = $item.find('.hero__resonse');
+	
+			if ( !$(item).hasClass('hero-item__wrapper--active') ) {
+				$(item).addClass('hero-item__wrapper--active');
+				outBlock.slideDown();
+				
+			} else {
+				$(item).removeClass('hero-item__wrapper--active');
+				outBlock.slideUp();
+			}
+
+			if ($(".hero-item__wrapper--active").length === 0) {
+				$('.hero').removeClass('hero--height');
+			} else {
+				$('.hero').addClass('hero--height');
+			}
+
+		}
 	}
 
 	$('.type-tabs__item').on('click', function(e) {
 		tabs(this);
+	});
+
+	$('.hero-item__wrapper').on('click', function() {
+		mobileChose(this);
 	});
 
 
