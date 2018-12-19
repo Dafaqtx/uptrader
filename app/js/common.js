70145d4c817225fd96152040de7681a3ad64101c
Alexander Kuchuk <dafaqtx@gmail.com>
2018-12-19 15:05:10 +0300

    add desktop version

--- app/js/common.js
+++ app/js/common.js
@@ -1,3 +1,23 @@
 $(function() {
-	/* some JS */
+	
+	function tabs(item) {
+		let $item       = $(item);
+		let listItems   = $('.type-tabs__item');
+		let index       = listItems.index($item);
+		let tabsItem    = $('.type-tabs__item');
+		let tabsContent = $('.type-tabs__content');
+
+		tabsItem.removeClass('type-tabs__item--active');
+		$item.addClass('type-tabs__item--active');
+
+		tabsContent.removeClass('type-tabs__content--active');
+		$('.type-tabs__content:eq(' + index + ')').addClass('type-tabs__content--active');
+
+	}
+
+	$('.type-tabs__item').on('click', function(e) {
+		tabs(this);
+	});
+
+
 })
